66a845d3751af6956952f3ec2774ceace1949866
Mahmoud Mohamed <mahmoudMohamed66@gmail.com>
2022-07-17 18:09:35 +0200

    fix issue in first_program

--- 2- second.py
+++ 2- second.py
@@ -1,9 +1,12 @@
-sentance = input("Enter the sentance: ")
+while True:
+    sentance = input("Enter the sentance: ")
 
-def count_words(sentance):
-    result = len(sentance.split(" ") )
+    def count_words(sentance):
+        result = len(set(sentance.split(" ")))
+        return result
 
-    return result
+    print(count_words(sentance))
 
-print(count_words(sentance))
-
+    choice = input("let's count anthor sentance, type any key to countinue or type 'x' to exit ")
+    if choice == 'x':
+        break
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Python3-tasks-Basics
+
+## 	this repo include 6 program:
+
+1. **First** to 
+2. **Second** is words counter without duplicate
+3. **Third** 
+4. **Fourth**
+5. **Fifth**
+6. **Sixth** is game ,you choice one of the games 
+
